a4a662faeda5cf2c1e4bd2294cca6638f54f346f
latifismail53 <latifismailadjie@gmail.com>
2023-07-16 13:59:31 +0700

    #revamp assets login

--- application/modules/Auth/views/login_view.php
+++ application/modules/Auth/views/login_view.php
@@ -3,23 +3,20 @@
 
 <head>
   <meta charset="utf-8">
-  <meta name="csrf_token" content="<?php echo $this->security->get_csrf_hash(); ?>">
   <meta name="viewport" content="width=device-width, initial-scale=1">
   <title>SIMA</title>
-  <link rel="shortcut icon" href="<?= base_url('assets/logo/logodm.png'); ?>" />
-  <link rel="icon" href="<?= base_url('assets/logo/logodm.png'); ?>" type="image/x-icon">
-  <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700&display=fallback">
-  <link rel="stylesheet" href="<?= base_url('assets/css/'); ?>font-awesome.all.min.css">
-  <link rel="stylesheet" href="<?= base_url('assets/css/'); ?>all.min.css">
-  <link rel="stylesheet" href="<?= base_url('assets/css/'); ?>adminlte.min.css">
-  <link rel="stylesheet" href="<?= base_url('assets/css/'); ?>bootstrap.min.css">
-  <link rel="stylesheet" href="<?= base_url('assets/css/'); ?>jquery-ui.min.css">
-  <link rel="stylesheet" href="<?= base_url('assets/css/'); ?>jquery-ui.css">
-  <link rel="stylesheet" href="<?= base_url('assets/css/'); ?>jquery.dataTables.min.css">
-  <link rel="stylesheet" href="<?= base_url('assets/css/'); ?>buttons.dataTables.min.css">
-  <link rel="stylesheet" href="<?= base_url('assets/css/'); ?>select.dataTables.min.css">
-  <link rel="stylesheet" href="<?= base_url('assets/css/'); ?>sweetalert2.min.css">
-  <link rel="stylesheet" href="<?= base_url('assets/css/'); ?>select2.min.css" />
+  <!-- Font Awesome -->
+  <link rel="stylesheet" href="<?= base_url() ?>assets/plugins/fontawesome-free/css/all.min.css">
+  <link rel="stylesheet" href="<?= base_url() ?>assets/plugins/fontawesome-free/css/fontawesome.min.css">
+  <link rel="stylesheet" href="<?= base_url() ?>assets/plugins/fontawesome-free/css/solid.min.css">
+  <link rel="stylesheet" href="<?= base_url() ?>assets/plugins/fontawesome-free/css/brands.min.css">
+  <!-- Ionicons -->
+  <link rel="stylesheet" href="https://code.ionicframework.com/ionicons/2.0.1/css/ionicons.min.css">
+  <!-- Theme Adminlte style -->
+  <link rel="stylesheet" href="<?= base_url() ?>assets/dist/css/adminlte.min.css">
+  <link rel="preconnect" href="https://fonts.googleapis.com">
+  <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+  <link href="https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@1,300&display=swap" rel="stylesheet">
 </head>
 
 <body class="sidebar-collapse login-page">
@@ -33,11 +30,11 @@
             </div>
             <div class="card-body">
               <?= form_open(base_url(md5('prelogin')), ['method' => 'post', 'autocomplete' => 'off']); ?>
-              <div class="input-group input-group-sm mb-3">
+              <div class="input-group mb-3">
                 <span class="input-group-text" id="basic-addon1"><i class="fas fa-user"></i></span>
                 <input type="text" name="username" class="form-control" id="inputUsername" autocomplete="false" placeholder="Username" required>
               </div>
-              <div class="input-group input-group-sm mb-2">
+              <div class="input-group mb-2">
                 <span class="input-group-text" id="basic-addon1"><i class="fas fa-key"></i></span>
                 <input type="password" name="password" id="password" class="form-control" autocomplete="false" placeholder="Password">
                 <button id="toggle-password" type="button" class="btn btn-outline-secondary"><i class="fas fa-eye"></i></button>
@@ -57,25 +54,20 @@
     </div>
   </div>
 </body>
-<script type="text/javascript" src="<?= base_url('assets/js/') ?>jquery-3.7.0.min.js"></script>
-<script type="text/javascript" src="<?= base_url('assets/js/') ?>jquery.dataTables.min.js"></script>
-<script type="text/javascript" src="<?= base_url('assets/js/') ?>popper.min.js"></script>
-<script type="text/javascript" src="<?= base_url('assets/js/') ?>bootstrap-5.2.3.js"></script>
-<script type="text/javascript" src="<?= base_url('assets/js/') ?>bootstrap.bundle.min.js"></script>
+<!-- jQuery -->
+<script type="text/javascript" src="<?= base_url('assets/plugins/jquery/') ?>jquery-3.7.0.min.js"></script>
+<!-- Bootstrap 4 -->
+<script type="text/javascript" src="<?= base_url('assets/plugins/bootstrap/js/') ?>bootstrap.bundle.min.js"></script>
+<!-- sweetalert2 -->
+<script type="text/javascript" src="<?= base_url('assets/plugins/sweetalerts/') ?>sweetalert2.all.min.js"></script>
+<!-- Select2 -->
+<script type="text/javascript" src="<?= base_url('assets/plugins/select2/'); ?>select2.min.js"></script>
+
+<!-- AdminLTE App -->
+<script type="text/javascript" src="<?= base_url('assets/dist/js/') ?>adminlte.min.js"></script>
+
+<!-- DateRangePicker -->
 <script type="text/javascript" src="<?= base_url('assets/js/') ?>jquery-ui.min.js"></script>
-<script type="text/javascript" src="<?= base_url('assets/js/') ?>highcharts.js"></script>
-<script type="text/javascript" src="<?= base_url('assets/js/') ?>data.js"></script>
-<script type="text/javascript" src="<?= base_url('assets/js/') ?>drilldown.js"></script>
-<script type="text/javascript" src="<?= base_url('assets/js/') ?>exporting.js"></script>
-<script type="text/javascript" src="<?= base_url('assets/js/') ?>export-data.js"></script>
-<script type="text/javascript" src="<?= base_url('assets/js/') ?>accessibility.js"></script>
-<script type="text/javascript" src="<?= base_url('assets/js/') ?>sweetalert2.all.min.js"></script>
-<script type="text/javascript" src="<?= base_url('assets/datatable/') ?>dataTables.buttons.min.js"></script>
-<script type="text/javascript" src="<?= base_url('assets/datatable/') ?>jszip.min.js"></script>
-<script type="text/javascript" src="<?= base_url('assets/datatable/') ?>buttons.html5.min.js"></script>
-<script type="text/javascript" src="<?= base_url('assets/datatable/') ?>buttons.print.min.js"></script>
-<script type="text/javascript" src="<?= base_url('assets/datatable/') ?>dataTables.select.min.js"></script>
-<script type="text/javascript" src="<?= base_url('assets/js/'); ?>select2.min.js"></script>
 <script>
   $(document).ready(function() {
     setInterval(() => {
